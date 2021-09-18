215ba7eca8bcda2ba7951784c5499fdae1cfd2c6
Elliot Newnham <elliot.newnham@gmail.com>
2021-09-18 16:30:21 -0400

    Add startup file

--- api/Program.cs
+++ api/Program.cs
@@ -1,28 +1,16 @@
-using Microsoft.OpenApi.Models;
+using api;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
+public class Program
 {
-    c.SwaggerDoc("v1", new() { Title = "api", Version = "v1" });
-});
+	public static void Main(string[] args)
+	{
+		CreateHostBuilder(args).Build().Run();
+	}
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
+	public static IHostBuilder CreateHostBuilder(string[] args) =>
+	    Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
